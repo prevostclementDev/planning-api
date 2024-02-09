c626f122ce3b415d3756290637ab1cf4404b5813
prevostclementDev <clementprevost45@gmail.com>
2024-02-09 18:25:30 +0100

    feat(db) .env file configuration test

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,17 @@
 
 namespace App\Controllers;
 
+use App\Models\TestModel;
+
 class Home extends BaseController
 {
     public function index(): string
     {
+
+        $testModel = new TestModel();
+
+        var_dump( $testModel->findAll() );
+
         return view('welcome_message');
     }
 }
