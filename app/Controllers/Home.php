ced811be48a6ea2239cc6717b395aa59eb1d49ea
prevostclementDev <clementprevost45@gmail.com>
2024-02-19 23:31:11 +0100

    workflow(global) : lasted test

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,7 +2,19 @@
 
 namespace App\Controllers;
 
-class Home
-{
+use App\Models\UserModelisation;
 
+class Home extends BaseController
+{
+    public function index(): void {
+
+        var_dump($_ENV['SECRET_KEY']);
+
+        $user = new UserModelisation();
+
+        var_dump($user->findAll());
+
+        dd('test');
+
+    }
 }
